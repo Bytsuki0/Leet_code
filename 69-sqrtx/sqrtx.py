ce7cdc4a43e3fed3c6d915a2bc7a2dbb843f5252
Gustavo Vitor da Silva <131626504+Bytsuki0@users.noreply.github.com>
2024-06-10 19:13:39 -0300

    Time: 1670 ms (5.11%) | Memory: 16.4 MB (97.05%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,4 +1,14 @@
 import math
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(sqrt(x))
+        i = 1
+        while True:
+            if i * i == x:
+                return i 
+                break
+            elif (i * i) < x:
+                i+= 1
+            elif (i * i) > x:
+                i-= 1
+                return i
+                break
