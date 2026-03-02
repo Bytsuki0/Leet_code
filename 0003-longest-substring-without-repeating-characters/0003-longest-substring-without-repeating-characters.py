4935f78024529311e288244292fcc42899c12bb0
Gustavo Vitor da Silva <131626504+Bytsuki0@users.noreply.github.com>
2026-03-02 19:37:02 -0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,21 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        last_seen = {}      # stores last index of each character
-        left = 0            # left pointer of sliding window
-        max_length = 0      # result
+        last_seen = {}   
+        left = 0            
+        max_length = 0      
         
         for right in range(len(s)):
             char = s[right]
             
-            # If character seen and inside current window
             if char in last_seen and last_seen[char] >= left:
                 left = last_seen[char] + 1
             
-            # Update last seen index
             last_seen[char] = right
             
-            # Update max length
             max_length = max(max_length, right - left + 1)
         
         return max_length
